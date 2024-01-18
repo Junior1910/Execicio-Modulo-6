4a8dbf63c259c600249495c11f921bf22ebb5bee
Florisvaldo <juniortcj10012@hotmail.com>
2024-01-18 20:23:28 -0300

    Feito o passo a passo de acordar até fazer o café

---
+++ algoritmo.java
@@ -0,0 +1,66 @@
+
+
+public class algoritmo {
+    public static void main(String[] args) {
+        System.out.println("1-Acordo.");
+        System.out.println("2-Abro os olhos.");
+        System.out.println("3-Levanto da cama.");
+        System.out.println("4-Acendo a luz do quarto");
+        System.out.println("5-Calço os chinelos.");
+        System.out.println("6-Agarro o celular.");
+        System.out.println("7-Desbloqueio a tela do celular.");
+        System.out.println("8-Acesso o aplicativo do Whatsapp.");
+        System.out.println("9-Olho as mensagens.");
+        System.out.println("10-Respondo as mensagens.");
+        System.out.println("11-Travo a tela novamente.");
+        System.out.println("12-Vou até a cozinha.");
+        System.out.println("13-Acendo a luz da cozinha.");
+        System.out.println("14-Vou até o armário de louça.");
+        System.out.println("15-Fecho a porta do armário");
+        System.out.println("16-Agarro a chaleira.");
+        System.out.println("17-Vou até filtro");
+        System.out.println("18-Tiro a tampa da chaleira.");
+        System.out.println("19-Abro a torneira do filtro.");
+        System.out.println("20-Fecho a torneira do filtro.");
+        System.out.println("21-Coloco a tampa na chaleira.");
+        System.out.println("23-Vou até o fogão");
+        System.out.println("24-Levanto o vidro do fogão .");
+        System.out.println("25-Coloco a chaleira em cima do fogão.");
+        System.out.println("26-Ligo a válvula de gás do botijão.");
+        System.out.println("27-Regulo a intensidade da chama.");
+        System.out.println("28-Aperto o botão acendedor.");
+        System.out.println("29-Desligo o fogo.");
+        System.out.println("30-Vou até a dispensa.");
+        System.out.println("31-Abro a porta da dispensa.");
+        System.out.println("32-Agarro o café.");
+        System.out.println("33-Fecho a porta da dispensa");
+        System.out.println("34-Vou até a pia");
+        System.out.println("35-Coloco o café em cima da pia.");
+        System.out.println("36-Vou até o fogão.");
+        System.out.println("37-Agarro a chaleira.");
+        System.out.println("38-Vou até a pia.");
+        System.out.println("39-Coloco a chaleira em cima da pia.");
+        System.out.println("40-Vou até o armário.");
+        System.out.println("41-Pego o cuador.");
+        System.out.println("42-Fecho a porta do armário");
+        System.out.println("43-Vou até a pia.");
+        System.out.println("44-Tiro a tampa da Chaleira.");
+        System.out.println("45-Coloco o cuador na chaleira.");
+        System.out.println("46-Pego uma colher");
+        System.out.println("47-Abro o pacote de café.");
+        System.out.println("48-Pego o café com colher");
+        System.out.println("49-Coloco o café no cuador");
+        System.out.println("50-Despejo a água no cuador");
+        System.out.println("51-Coloco a tampa na chaleira");
+        System.out.println("52-Vou até o armário");
+        System.out.println("53-Abbro a porta do armário");
+        System.out.println("54-Agarro a garrafa de café.");
+        System.out.println("55-Fecho a porta do armário");
+        System.out.println("56-Vou até a pia");
+        System.out.println("57-Tiro a tampa da garrafa de café");
+        System.out.println("58-Agarro a chaleira");
+        System.out.println("59-Despejo o café na garrafa.");
+        System.out.println("60-Coloco a tampa na garrafa de café");
+        }
+    }
+        
